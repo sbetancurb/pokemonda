0bb56134b4862e37fdab583729e9ee79e06ff0ad
sbetancurb <sbetancurb@eafit.edu.co>
2023-11-07 06:44:47 -0500

    Pokeneas

--- public/util.js
+++ public/util.js
@@ -3,29 +3,21 @@ function capitalize(text) {
 }
 
 $(document).ready(function () {
-    const pokeNameCard = document.getElementById('pokeNameCard');
-    const pokeHeightCard = document.getElementById('pokeHeightCard');
-    const pokePhraseCard = document.getElementById('pokePhraseCard');
-    const pokeAbilityCard = document.getElementById('pokeAbilityCard');
-    const containerIdCard = document.getElementById('containerIdCard');
-    const pokeImgCard = document.getElementById('pokeImgCard');
-    const pokeNameOut = document.getElementById('pokeNameOut');
-    const pokeImgOut = document.getElementById('pokeImgOut');
 
     $('#randomize').on('click', function () {
         document.getElementById('pokeCard').removeAttribute('hidden');
         $.ajax({
             type: 'GET',
-            url: "/api/randomPokemon",
+            url: '/api/randomPokemon',
             success: function (data) {
-                pokeNameCard.innerText = capitalize(data['nombre']);
-                pokeHeightCard.innerText = capitalize(data['altura']);
-                pokePhraseCard.innerText = capitalize(data['frase']);
-                pokeAbilityCard.innerText = capitalize(data['habilidad']);
-                containerIdCard.innerText = data['hostId'];
-                pokeNameOut.innerText = capitalize(data['nombre']);
-                pokeImgCard.setAttribute('src', data['imagen']);
-                pokeImgOut.setAttribute('src', data['imagen']);
+                document.getElementById('pokeNameCard').innerText = capitalize(data['nombre']);
+                document.getElementById('pokeHeightCard').innerText = capitalize(data['altura']);
+                document.getElementById('pokePhraseCard').innerText = capitalize(data['frase']);
+                document.getElementById('pokeAbilityCard').innerText = capitalize(data['habilidad']);
+                document.getElementById('containerIdCard').innerText = data['hostId'];
+                document.getElementById('pokeNameOut').innerText = capitalize(data['nombre']);
+                document.getElementById('pokeImgCard').setAttribute('src', data['imagen']);
+                document.getElementById('pokeImgOut').setAttribute('src', data['imagen']);
             },
             error: function (xhr, status, error) {
                 console.error(error);
